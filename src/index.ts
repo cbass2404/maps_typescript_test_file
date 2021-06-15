147310d6c89c043bd19a8fc02aa8a8c08fb7eb8d
Cory Bass <cory.bass.07@gmail.com>
2021-06-15 14:19:11 -0500

    refactored addmarker methods to consolidate code

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ import { User } from './User';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
-const map = new CustomMap('map');
+const company = new Company();
+const user = new User();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
